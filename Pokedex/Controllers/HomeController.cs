2d95974261cbda4c0ee84fd181762318998a913f
Maria Clara Carvalho <mclara18062005@gmail.com>
2023-03-07 11:26:25 -0300

    Refatoração do código ler arquivo e exibição dinâmica das cores dos botões tipos

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -16,15 +16,24 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        string arquivo = @"Data\pokemons.json";
-        using (StreamReader leitor = new StreamReader(arquivo)) 
+        var tipos = JsonSerializer.Deserialize<List<Tipo>>(
+            LerArquivo(@"Data\tipos.json")
+        );
+        ViewData["Tipos"] = tipos;
+         var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(
+             LerArquivo(@"Data\pokemons.json")
+         );
+            return View(pokemons);
+        
+    }
+
+    private string LerArquivo(string nomeArquivo)
+    {
+        using (StreamReader leitor = new StreamReader(nomeArquivo)) 
         {
             string dados = leitor.ReadToEnd(); 
-            var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
-            return View(pokemons);
-
+            return dados;
         }
-       
     }
 
     public IActionResult Privacy()
